c1eb2ef5dd1efcd234786f73cebeb6d153d9de10
JeremYD <delpech.jeremy@hotmail.fr>
2020-11-02 16:04:54 +0100

    Add Font Awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { DivisibleComponent } from './divisible/divisible.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { MatCardModule } from '@angular/material/card';
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
